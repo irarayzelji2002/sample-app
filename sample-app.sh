a728496758c446eddc75af621b17093704148c52
Ira Rayzel S. Ji <irarayzelji@gmail.com>
2024-12-09 18:33:26 +0000

    Changed port from 8080 to 5050 for Jenkins.

--- sample-app.sh
+++ sample-app.sh
@@ -15,13 +15,13 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 # Building Docker container
 cd tempdir
-docker build -t sampleapp .
+docker build -t sampleapp-jenkins .
 
 # Starting Docker container
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
+docker run -t -d -p 5050:5050 --name samplerunning-jenkins sampleapp-jenkins
 docker ps -a
